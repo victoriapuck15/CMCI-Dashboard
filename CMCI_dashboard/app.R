11962bb0791807082a6c4833b805c76d7ab284b0
victoriapuck15 <victoriapuck15@gmail.com>
2024-03-17 14:43:54 -0400

    R Shiny Dashboard

---
+++ CMCI_dashboard/app.R
@@ -0,0 +1,51 @@
+# Load necessary libraries
+library(shiny)
+library(ggplot2)
+library(dplyr)
+library(readr)
+
+# Read the data from CSV file
+data <- read.csv("/Users/victoriapuck-karam/Documents/CMCI-Dashboard/CMCI_FINAL_DATA.csv")
+
+# Convert the 'DATE' column to date format
+data$DATE <- as.Date(data$DATE)
+
+# Define UI
+ui <- fluidPage(
+  titlePanel("CMCI Time Series"),
+  sidebarLayout(
+    sidebarPanel(
+      dateRangeInput("date_range", "Date Range:",
+                     start = min(data$DATE),
+                     end = max(data$DATE),
+                     min = min(data$DATE),
+                     max = max(data$DATE))
+    ),
+    mainPanel(
+      plotOutput("linePlot")
+    )
+  )
+)
+
+# Define server logic
+
+  # Define server logic
+  server <- function(input, output) {
+    
+    # Reactive expression for data subset based on date range
+    selectedData <- reactive({
+      data %>%
+        filter(DATE >= input$date_range[1] & DATE <= input$date_range[2])
+    })
+    
+    # Generate the line plot
+    output$linePlot <- renderPlot({
+      ggplot(selectedData(), aes(x = DATE, y = CMCI)) +
+        geom_line() +
+        labs(x = "Date", y = "CMCI", title = "CMCI Time Series") +
+        theme(axis.text.x = element_text(angle = 45, hjust = 1))
+    })
+  }
+  
+# Run the application
+shinyApp(ui = ui, server = server)
