18a45608f4484035eaa32c699780cc58f58aa676
Michael Licata <20mlicata@gmail.com>
2024-03-17 17:11:04 -0400

    Added two new dashboard features

--- CMCI_dashboard/app.R
+++ CMCI_dashboard/app.R
@@ -3,31 +3,58 @@ library(shiny)
 library(ggplot2)
 library(dplyr)
 library(readr)
+library(plotly)
 
 # Read the data from CSV file
-data <- read.csv("/Users/victoriapuck-karam/Documents/CMCI-Dashboard/CMCI_FINAL_DATA.csv")
+data2 <- read.csv("/Users/michaellicata/Documents/SeniorYear/SP '24/DS 440/CMCI-Dashboard/cmciGood2.csv")
+compare_df <- read.csv("/Users/michaellicata/Documents/SeniorYear/SP '24/DS 440/CMCI-Dashboard/compare_data.csv")
+colnames(data2)[1] <- "DATE"
 
-# Comment to test git
 
 # Convert the 'DATE' column to date format
-data$DATE <- as.Date(data$DATE)
+data2$DATE <- as.Date(data2$DATE)
+compare_df$DATE <- as.Date(compare_df$DATE)
 
 # Define UI
 ui <- fluidPage(
   titlePanel("CMCI Time Series"),
   sidebarLayout(
     sidebarPanel(
-      dateRangeInput("date_range", "Date Range:",
-                     start = min(data$DATE),
-                     end = max(data$DATE),
-                     min = min(data$DATE),
-                     max = max(data$DATE))
-    ),
+      tabsetPanel(
+        tabPanel("CMCIPlot",
+                 
+                 dateRangeInput("date_range", "Date Range:",
+                     start = min(data2$DATE),
+                     end = max(data2$DATE),
+                     min = min(data2$DATE),
+                     max = max(data2$DATE)),
+                 
+                 hr(),
+                 
+                 checkboxGroupInput("countries", "Select Countries:", 
+                         choices = colnames(data2)[-1], selected = colnames(data2)[-1])
+      ),
+      tabPanel("ComparisonPlot",
+               
+               dateRangeInput("date_range2", "Date Range:",
+                              start = min(compare_df$DATE),
+                              end = max(compare_df$DATE),
+                              min = min(compare_df$DATE),
+                              max = max(compare_df$DATE)),
+               
+               hr(),
+               
+               checkboxGroupInput("indexes", "Select Indexes:", 
+                                  choices = colnames(compare_df)[-1], selected = colnames(compare_df)[-1])
+      )
+      )
+      ),
     mainPanel(
-      plotOutput("linePlot"),
+      plotlyOutput("CMCIPlot"),
+      plotlyOutput("ComparisonPlot"),
       textOutput("dataInfo")
-    )
   )
+)
 )
 
 # Define server logic
@@ -35,22 +62,50 @@ server <- function(input, output) {
   
   # Reactive expression for data subset based on date range
   selectedData <- reactive({
-    data %>%
-      filter(DATE >= input$date_range[1] & DATE <= input$date_range[2])
+    data2 %>%
+      filter(DATE >= input$date_range[1] & DATE <= input$date_range[2]) %>%
+      select(c("DATE", input$countries))
   })
   
-  # Generate the line plot
-  output$linePlot <- renderPlot({
-    ggplot(selectedData(), aes(x = DATE, y = CMCI)) +
-      geom_line() +
-      labs(x = "Date", y = "CMCI", title = "CMCI Time Series") +
-      theme(axis.text.x = element_text(angle = 45, hjust = 1))
+  # Reactive expression for data subset based on date range for ComparisonPlot
+  selectedData2 <- reactive({
+    compare_df %>%
+      filter(DATE >= input$date_range2[1] & DATE <= input$date_range2[2]) %>%
+      select(c("DATE", input$indexes))
   })
   
-  # Render text explaining the data
-  output$dataInfo <- renderText({
-    "This is a placeholder for where the educational information will go and the data transparency statements"
+  output$CMCIPlot <- renderPlotly({
+    # Filter data based on selected countries
+    dataSelected <- selectedData()
+    
+    # Convert to long format for plotting with plotly
+    dataLong <- reshape2::melt(dataSelected, id.vars = "DATE", variable.name = "Country", value.name = "Index")
+    
+    # Generate plot
+    p <- plot_ly(dataLong, x = ~DATE, y = ~Index, color = ~Country, type = 'scatter', mode = 'lines') %>%
+      layout(title = "Country Index Over Time",
+             xaxis = list(title = "Date"),
+             yaxis = list(title = "Index"))
+    
+    return(p)
   })
+  
+  output$ComparisonPlot <- renderPlotly({
+    # Filter data based on selected countries
+    dataSelected <- selectedData2()
+    
+    # Convert to long format for plotting with plotly
+    dataLong <- reshape2::melt(dataSelected, id.vars = "DATE", variable.name = "Index", value.name = "Value")
+    
+    # Generate plot
+    p <- plot_ly(dataLong, x = ~DATE, y = ~Value, color = ~Index, type = 'scatter', mode = 'lines') %>%
+      layout(title = "Country Index Over Time",
+             xaxis = list(title = "Date"),
+             yaxis = list(title = "Value of Index"))
+    
+    return(p)
+  })
+  
 }
 
 # Run the application
